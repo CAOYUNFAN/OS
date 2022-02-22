a518f2610459c577ac33c890a40fd1c080fb9a8a
tracer-nju <tracer@nju.edu.cn>
2022-02-23 04:03:09 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:03:09 up 25 min,  1 user,  load average: 0.71, 0.60, 0.49

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,63 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include <stdlib.h>
+#include <getopt.h>
+
+const struct option table[]={
+  {"show-pids"    ,no_argument  ,NULL ,'p'},
+  {"numeric-sort" ,no_argument  ,NULL ,'n'},
+  {"version"      ,no_argument  ,NULL ,'V'},
+  {0              ,0            ,0    ,0}
+};
+
+int flag_n,flag_p;
+void prase_args(int argc,char * argv[]){
+  int o;
+  while ((o=getopt_long(argc,argv,"-pnV",table,NULL))!=-1){
+    switch (o) {
+    case 'V':
+      fprintf(stderr,"pstree (for OS) 0.0\n");
+      exit(0);
+    case 'n':
+      flag_n=1;printf("-n!\n");
+      break;
+    case 'p':
+      flag_p=1;printf("-p!\n");
+      break;
+    default:
+      fprintf(stderr,"Usage : pstree [OPTION]\n");
+      fprintf(stderr,"\t-V,--version\n");
+      fprintf(stderr,"\t-n,--numeric-sort\n");
+      fprintf(stderr,"\t-p,--show-pids\n");
+      exit(1);
+    }
+  }
+  return;
+}
+
+#define N 10000
+#define M 100
+int n;
+pid_t pid[N],fa[N];
+char name[N][M];
+
+/*void work(){
+  DIR dir=open_dir("/proc/")
+  return;
+}*/
+
 int main(int argc, char *argv[]) {
+  prase_args(argc,argv);
+//  work();
+
+  return 0;
+}
+
+/*
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
-  return 0;
-}
+*/

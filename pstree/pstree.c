2a10d0f280e14c5970ad288a61aa8e25bd2295a7
tracer-nju <tracer@nju.edu.cn>
2022-02-23 16:15:34 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:15:34 up 15 min,  1 user,  load average: 0.32, 0.36, 0.29

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -80,7 +80,7 @@ int cmp(const unit a,const unit b){return a.pid<b.pid;}
 int main(int argc, char *argv[]) {
   prase_args(argc,argv);
   work();
-  if(flag_n) sort(a,a+n,cmp);
+  if(flag_n) qsort(a,a+n,cmp);
   return 0;
 }
 

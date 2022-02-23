5418c5022b4384f460c0fde8147f8e858a829d91
tracer-nju <tracer@nju.edu.cn>
2022-02-23 18:34:14 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:34:14 up 22 min,  1 user,  load average: 1.00, 0.59, 0.39

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -76,9 +76,9 @@ void work(){
       if(ch==')') --flag;
     }
     for(ch=fgetc(fp);ch!=' ';ch=fgetc(fp));
-    fscanf(fp,"%d",a[n].fa);
-    printf("%d %s %d\n",a[n].pid,a[n].name,a[n].fa);
-    if(a[n].name[0]=='(') printf("%d:%s\n",n,a[n].name);
+    fscanf(fp,"%d",&a[n].fa);
+//    printf("%d %s %d\n",a[n].pid,a[n].name,a[n].fa);
+//    if(a[n].name[0]=='(') printf("%d:%s\n",n,a[n].name);
     fclose(fp);
     n++;
   }
@@ -90,10 +90,10 @@ int cmp(const void * a,const void * b){return ((unit *)a)->pid>((unit *)b)->pid;
 
 void dfs(pid_t fa,int len){
   for(int i=0;i<n;++i) if(a[i].fa==fa){
-//    for(int i=0;i<len;++i) putchar(' ');
-//    printf("%s",a[i].name);
-//    if(flag_p) printf("(%d)",a[i].pid);
-//    putchar('\n');
+    for(int i=0;i<len;++i) putchar(' ');
+    printf("%s",a[i].name);
+    if(flag_p) printf("(%d)",a[i].pid);
+    putchar('\n');
     dfs(a[i].pid,len+2);
   }
 }

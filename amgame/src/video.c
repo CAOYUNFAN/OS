e12e6162605ba985a19defc591e83b575464fdef
tracer-nju <tracer@nju.edu.cn>
2022-02-27 07:13:25 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:13:25 up 59 min,  1 user,  load average: 0.67, 0.39, 0.45

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -29,7 +29,7 @@ void splash() {
   if(posy<0) posy=0;
   if(posy>=h-4*SIDE) posy=h-4*SIDE;
   draw_tile(posx,posy,4*SIDE,4*SIDE,0xffffff);
-//  printf("%d %d\n",posx,posy);
+  printf("%d %d\n",posx,posy);
 /*  for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {

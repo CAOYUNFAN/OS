ea50327d6632f8ca4525065d467d6a96f44924c5
tracer-nju <tracer@nju.edu.cn>
2022-02-27 07:05:49 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:05:49 up 52 min,  1 user,  load average: 0.39, 0.56, 0.56

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,7 +1,7 @@
 #include <game.h>
 
 // Operating system is a C program!
-#define FPS 250
+#define FPS 20
 
 inline uint64_t uptime(){
   AM_TIMER_UPTIME_T ret;
@@ -16,20 +16,15 @@ int main(const char *args) {
 //  puts(args); // make run mainargs=xxx
 //  puts("\"\n");
 //  printf("HERE!\n");
-  splash();
-
-  puts("Press any key to see its key code...\n");
+ 
+  uint64_t next_frame=0;
   while (1) {
-    func_key();
-  }
-//  uint64_t next_frame=0;
-/*  while (1) {
 //    printf("A!\n");
     while(uptime()<next_frame);
     func_key();
     splash();
     next_frame+=1000/FPS;
-  }*/
+  }
   return 0;
 }
 /*

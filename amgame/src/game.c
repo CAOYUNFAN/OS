72376591d46ddb6b7763c6988004880b8efed39b
tracer-nju <tracer@nju.edu.cn>
2022-02-27 07:02:45 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:02:45 up 48 min,  1 user,  load average: 0.39, 0.65, 0.59

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -16,14 +16,20 @@ int main(const char *args) {
 //  puts(args); // make run mainargs=xxx
 //  puts("\"\n");
 //  printf("HERE!\n");
-  uint64_t next_frame=0;
+  splash();
+
+  puts("Press any key to see its key code...\n");
   while (1) {
+    func_key();
+  }
+//  uint64_t next_frame=0;
+/*  while (1) {
 //    printf("A!\n");
     while(uptime()<next_frame);
     func_key();
     splash();
     next_frame+=1000/FPS;
-  }
+  }*/
   return 0;
 }
 /*

c16adf42ba056b2f557c1b89d1fd1cf747b83165
tracer-nju <tracer@nju.edu.cn>
2022-02-27 06:49:24 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:49:24 up 35 min,  1 user,  load average: 1.31, 0.64, 0.45

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -5,14 +5,20 @@
 static const char *key_names[] = {
   AM_KEYS(KEYNAME)
 };
-
+int posx,posy;
 void func_key() {
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown) {
     if(event.keycode==1) halt(0);
-    puts("Key pressed: ");
-    printf("%d %s",event.keycode,key_names[event.keycode]);
-    puts("\n");
+    if(strcmp(key_names[event.keycode],"A")==0) posy-=16;
+    if(event.keycode==43) posx-=16;
+    if(event.keycode==44) posy+=16;
+    if(event.keycode==45) posx+=16;
   }
 }
+/*
+puts("Key pressed: ");
+    printf("%d %s",event.keycode,key_names[event.keycode]);
+    puts("\n");
+*/
--- amgame/src/video.c
+++ amgame/src/video.c
@@ -2,14 +2,13 @@
 
 #define SIDE 16
 static int w, h;
-
 static void init() {
   AM_GPU_CONFIG_T info = {0};
   ioe_read(AM_GPU_CONFIG, &info);
   w = info.width;
   h = info.height;
 }
-
+extern int posx,posy;
 static void draw_tile(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
   AM_GPU_FBDRAW_T event = {
@@ -22,13 +21,19 @@ static void draw_tile(int x, int y, int w, int h, uint32_t color) {
   ioe_write(AM_GPU_FBDRAW, &event);
 }
 
+#define Min(a,b) ((a)<(b)?(a):(b))
 void splash() {
   init();
-  for (int x = 0; x * SIDE <= w; x ++) {
+  if(posx<0) posx=0;
+  if(posx>=w-4*SIDE) posx=w-4*SIDE;
+  if(posy<0) posy=0;
+  if(posy>=h-4*SIDE) posy=h-4*SIDE;
+  draw_tile(posx,posy,4*SIDE,4*SIDE,0xffffff);
+  /*for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
         draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       }
     }
-  }
+  }*/
 }

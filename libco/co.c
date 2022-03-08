f7b2b2ec56512df33b7766ef60cb7906c38da432
tracer-nju <tracer@nju.edu.cn>
2022-03-09 01:52:51 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:52:51 up  4:13,  2 users,  load average: 0.61, 0.48, 0.38

--- libco/co.c
+++ libco/co.c
@@ -80,7 +80,7 @@ static void __co_yield(){
 
 static volatile void stack_switch_call(void * sp, void *entry, uintptr_t arg) {
   sp=(void *)( ((uintptr_t) sp & -16) );
-//  DEBUG("%p %p %p\n",(void *)sp,entry,(void *)arg);
+  DEBUG("%p %p %p\n",(void *)sp,entry,(void *)arg);
   asm volatile (
 #if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; call *%1"

51336be2504cf8beb13cd305ac564135b65b4cff
tracer-nju <tracer@nju.edu.cn>
2022-03-08 18:51:23 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:51:23 up 39 min,  1 user,  load average: 0.32, 0.47, 0.44

--- libco/co.c
+++ libco/co.c
@@ -72,14 +72,14 @@ void for_running(struct co *co){
 }
 
 static inline void stack_switch_call(void * sp, void *entry, uintptr_t arg) {
-  sp=(void *)( ((uintptr_t) sp & -16) +8 );
+  sp=(void *)( ((uintptr_t) sp & -16) );
 //  DEBUG("%p %p %p\n",(void *)sp,entry,(void *)arg);
   asm volatile (
 #if __x86_64__
-    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+    "movq %0, %%rsp; movq %2, %%rdi; call *%1"
       : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
 #else
-    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+    "movl %0, %%esp; movl %2, 4(%0); call *%1"
       : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
 #endif
   );

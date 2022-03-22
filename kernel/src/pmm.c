422e5e61bad1205ebf8418cf12922379d2d2cf6e
tracer-nju <tracer@nju.edu.cn>
2022-03-23 05:38:43 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:38:43 up  1:53,  1 user,  load average: 0.42, 0.62, 0.69

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -9,11 +9,12 @@
 
 
 #define MAX_malloc (16*1024*1024)
-#define Unit_size (MAX_malloc*2)
+#define Unit_size (MAX_malloc)
 #define Unit_mask (-Unit_size)
 
 #define HEAP_START ROUNDUP((uintptr_t)heap.start,Unit_size)
 #define HEAP_END ROUNDDOWN((uintptr_t)heap.end,Unit_size)
+#define total_num ((HEAP_END-HEAP_START)/Unit_size)
 
 #define LOWBIT(x) ((x)&((x)^((x)-1)))
 
@@ -68,8 +69,8 @@ static inline size_t up_bound(size_t size){
 free_list * start_of_128;
 static uintptr_t heap_128_start,heap_128_end;
 int lock_128;
-void init_128(){
-  heap_128_start=HEAP_START;heap_128_end=(HEAP_END+HEAP_START)>>1;
+static inline void init_128(){
+  heap_128_start=HEAP_START;heap_128_end=HEAP_START+total_num/2*Unit_size;
   start_of_128=(free_list *)heap_128_start;
   ((free_list *)heap_128_start)->size=128;
   for(uintptr_t ptr=heap_128_start+128;ptr<heap_128_end;ptr+=128){
@@ -79,7 +80,7 @@ void init_128(){
   ((free_list *)(heap_128_end-128))->nxt=NULL;
   return;
 }
-void * kalloc_128(size_t size){
+static inline void * kalloc_128(){
   if(start_of_128==NULL) return NULL;
   spin_lock(&lock_128);
   void * ret=(void *) start_of_128;
@@ -89,7 +90,7 @@ void * kalloc_128(size_t size){
   spin_unlock(&lock_128);
   return ret;
 }
-void kfree_128(void * ptr){
+static inline void kfree_128(void * ptr){
   free_list * hdr=ptr;
   hdr->size=128;
   spin_lock(&lock_128);
@@ -98,13 +99,44 @@ void kfree_128(void * ptr){
   spin_unlock(&lock_128);
 }
 
-void init_4096(){
 
+free_list * start_of_4096;
+static uintptr_t heap_4096_start,heap_4096_end;
+int lock_4096;
+static inline void init_4096(){
+  heap_4096_start=heap_128_end;
+  heap_4096_end=heap_4096_start+total_num/4*Unit_size;
+  start_of_4096=(free_list *)heap_4096_start;
+  ((free_list *)heap_4096_start)->size=4096;
+  for(uintptr_t ptr=heap_4096_start+4096;ptr<heap_4096_end;ptr+=4096){
+    ((free_list *)ptr)->size=4096;
+    ((free_list *)(ptr-4096))->nxt=(free_list *)ptr;
+  }
+  ((free_list *)(heap_4096_end-4096))->nxt=NULL;
+  return;
+}
+static inline void * kalloc_4096(){
+  if(start_of_4096==NULL) return NULL;
+  spin_lock(&lock_4096);
+  void * ret=(void *) start_of_4096;
+  if(ret){
+    start_of_4096=start_of_4096->nxt;
+  }
+  spin_unlock(&lock_4096);
+  return ret;
+}
+static inline void kfree_4096(void * ptr){
+  free_list * hdr=ptr;
+  hdr->size=4096;
+  spin_lock(&lock_4096);
+  hdr->nxt=start_of_4096->nxt;
+  start_of_4096=hdr;
+  spin_unlock(&lock_4096);
 }
 
-void init_mm(){
-  sbrk_now=HEAP_END;
-  init_128();
+uintptr_t heap_rest_start,heap_rest_end;
+void init_rest(){
+  heap_rest_start=heap_4096_end;heap_rest_end=HEAP_END-Unit_size;
 /*  num_of_block=(HEAP_END-HEAP_START)/Unit_size;
   for(uintptr_t i=HEAP_START,j=0;i<HEAP_END;i+=Unit_size,j++){
 //    printf("%lx,%lx,i=%lx,j=%d,num=%d\n",HEAP_USE_START,HEAP_END,i,j,num_of_block);
@@ -115,6 +147,48 @@ void init_mm(){
     ((free_list *)i)->nxt=NULL;
     *lock_addr(j)=MAGIC_UNLOCKED;
   }*/
+}
+static inline void * kalloc_rest(size_t size){
+  uintptr_t ret=0;
+  spin_lock(&lock);
+
+  spin_unlock(&lock);
+  return (void *)ret;
+
+
+/*  size=up_bound(size+sizeof(mem_tag));
+  uintptr_t try_num=0;
+  for(uintptr_t i=0,j=HEAP_START;try_num<num_of_block;i=(i+1)%num_of_block,j=(j+Unit_size==HEAP_END?HEAP_START:j+HEAP_END))
+  if(atomic_xchg(lock_addr(i),MAGIC_LOCKED)==MAGIC_UNLOCKED){
+    ++try_num;
+    void * ret=find(size);
+    if(ret) work(ret,size);
+    spin_unlock(lock_addr(i));
+    if(ret) return ret;
+  }
+  return NULL;*/
+}
+static inline void kfree_rest(void * ptr){
+/*  uintptr_t pos=(ROUNDDOWN((uintptr_t)ptr,Unit_size)-HEAP_START)/Unit_size;
+  spin_lock(lock_addr(pos));
+  real_free((uintptr_t)ptr);
+  spin_unlock(lock_addr(pos));*/
+}
+
+static inline void real_free(uintptr_t ptr){
+  spin_lock(&lock);
+  /*uintptr_t len=LOWBIT(ptr);
+  for(;len;len>>=1){
+    uintptr_t pos=ptr+len-sizeof(mem_tag);
+    if(((mem_tag *)pos)->size+ptr==pos+sizeof(mem_tag)&&((mem_tag *)pos)->magic==MAGIC_MHD) break;
+  }
+  memset((void *)ptr,len,MAGIC_UNUSED);*/
+
+}
+
+void init_mm(){
+  sbrk_now=HEAP_END;
+  init_128();init_4096();init_rest();
 }
 
 #ifndef TEST
@@ -151,49 +225,17 @@ static inline void work(void * ptr,size_t len){
   return;
 }
 
-static inline void * kalloc_case3(size_t size){
-  uintptr_t ret=0;
-  spin_lock(&lock);
-
-  spin_unlock(&lock);
-  return (void *)ret;
-}
-
 static void * kalloc(size_t size){
   if(size>MAX_malloc) return NULL;
-  if(size<=128) return kalloc_128(size);
-  return kalloc_case3(size);
-
-/*  size=up_bound(size+sizeof(mem_tag));
-  uintptr_t try_num=0;
-  for(uintptr_t i=0,j=HEAP_START;try_num<num_of_block;i=(i+1)%num_of_block,j=(j+Unit_size==HEAP_END?HEAP_START:j+HEAP_END))
-  if(atomic_xchg(lock_addr(i),MAGIC_LOCKED)==MAGIC_UNLOCKED){
-    ++try_num;
-    void * ret=find(size);
-    if(ret) work(ret,size);
-    spin_unlock(lock_addr(i));
-    if(ret) return ret;
-  }
-  return NULL;*/
-}
-
-static inline void real_free(uintptr_t ptr){
-  spin_lock(&lock);
-  /*uintptr_t len=LOWBIT(ptr);
-  for(;len;len>>=1){
-    uintptr_t pos=ptr+len-sizeof(mem_tag);
-    if(((mem_tag *)pos)->size+ptr==pos+sizeof(mem_tag)&&((mem_tag *)pos)->magic==MAGIC_MHD) break;
-  }
-  memset((void *)ptr,len,MAGIC_UNUSED);*/
-
+  if(size<=128) return kalloc_128();
+  if(size<=4096) return kalloc_4096();
+  return kalloc_rest(size);
 }
 
 static void kfree(void * ptr){
   if((uintptr_t)ptr>=heap_128_start&&(uintptr_t)ptr<heap_128_end) kfree_128(ptr);
-/*  uintptr_t pos=(ROUNDDOWN((uintptr_t)ptr,Unit_size)-HEAP_START)/Unit_size;
-  spin_lock(lock_addr(pos));
-  real_free((uintptr_t)ptr);
-  spin_unlock(lock_addr(pos));*/
+  if((uintptr_t)ptr>=heap_4096_start&&(uintptr_t)ptr<heap_4096_end) kfree_4096(ptr);
+  if((uintptr_t)ptr>=heap_rest_start&&(uintptr_t)ptr<heap_rest_end) kfree_rest(ptr);
 }
 
 MODULE_DEF(pmm) = {

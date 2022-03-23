d803542a62bf8721d3bb08379622a304d8e79fd9
tracer-nju <tracer@nju.edu.cn>
2022-03-24 01:10:20 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:10:20 up  2:14,  1 user,  load average: 0.53, 0.48, 0.64

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -160,7 +160,6 @@ static inline void insert(free_list * insert,free_list ** head){
   return;
 }
 static inline void * kalloc_rest(size_t size){
-  printf("ENTERING_MALLOC!\n");
   size+=sizeof(mem_tag);
   free_list * ret=NULL;
   spin_lock(&lock_rest);
@@ -178,13 +177,12 @@ static inline void * kalloc_rest(size_t size){
     break;
   }
   spin_unlock(&lock_rest);
-  printf("EXIT!\n");
   return (void *)ret;
 }
 static inline free_list * update(free_list ** head){
   if(*head==NULL) return NULL;
   uintptr_t len=(*head)->size;
-  if(len==MAX_malloc) return NULL;
+  if(len==MAX_malloc) return NULL;printf("%ld\n",len);
   if(LOWBIT((uintptr_t)*head)>len&&(uintptr_t)*head+len==(uintptr_t)((*head)->nxt)){
     free_list * ret=*head;
     *head=(*head)->nxt->nxt;
@@ -202,7 +200,6 @@ static inline free_list * update(free_list ** head){
   return NULL;
 }
 static inline void kfree_rest(void * ptr){
-  printf("ENTERING_FREE!\n");
   uintptr_t len=LOWBIT((uintptr_t)ptr);
   for(;len;len>>=1){
     if(MTG_addr(ptr,len)->magic==MAGIC_MTG&&MTG_addr(ptr,len)->size==len) break;
@@ -214,7 +211,6 @@ static inline void kfree_rest(void * ptr){
   insert(ptr,&start_of_rest[pos]);
   while((ptr=update(&start_of_rest[pos]))!=NULL) insert(ptr,&start_of_rest[++pos]);
   spin_unlock(&lock_rest);
-  printf("EXIT!\n");
 }
 
 void init_mm(){

9c315f0b64e8eca185c22c05f8ad1794bc00c0e5
tracer-nju <tracer@nju.edu.cn>
2022-03-24 01:25:18 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:25:18 up  2:29,  1 user,  load average: 0.65, 0.88, 0.81

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -208,6 +208,7 @@ static inline void kfree_rest(void * ptr){printf("ENTERING free!\n");
   memset((void *)ptr,len,MAGIC_UNUSED);
   int pos=0;
   for(uintptr_t i=8192;i<len;i<<=1) ++pos;
+  printf("%d\n",pos);
   spin_lock(&lock_rest);
   insert(ptr,&start_of_rest[pos]);
   while((ptr=update(&start_of_rest[pos]))!=NULL) insert(ptr,&start_of_rest[++pos]),printf("pos=%d\n",pos);

40f1b7be8cfc44751ac26373dc1d36877f900a8c
tracer-nju <tracer@nju.edu.cn>
2022-03-24 01:16:26 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:16:26 up  2:20,  1 user,  load average: 1.11, 0.75, 0.70

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -173,7 +173,7 @@ static inline void * kalloc_rest(size_t size){
       divide->size=i;
       insert(divide,&start_of_rest[--j]);
     }
-    MTG_addr(ret,i)->size=i;MTG_addr(ret,i)->magic=MAGIC_MTG;printf("alloc_inside:%p\n",MTG_addr(ret,i));
+    MTG_addr(ret,i)->size=i;MTG_addr(ret,i)->magic=MAGIC_MTG;printf("alloc_inside:%p,%p\n",MTG_addr(ret,i),MTG_addr(ret,i)+sizeof(mem_tag));
     break;
   }
   spin_unlock(&lock_rest);

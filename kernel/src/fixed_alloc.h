e9bb14da01e385393bbee75393f51dc42e8b822e
tracer-nju <tracer@nju.edu.cn>
2022-03-25 03:02:45 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:02:45 up  3:20,  1 user,  load average: 0.80, 0.79, 0.75

--- kernel/src/fixed_alloc.h
+++ kernel/src/fixed_alloc.h
@@ -22,7 +22,7 @@
   }
 #define CAO_ALLOC(x)\
   static inline void * contact(kalloc_,x)(){\
-    if(start(x)==NULL) return NULL;\
+    if(head(x)==NULL) return NULL;\
     spin_lock(&lock(x));\
     void * ret=(void *) head(x);\
     if(ret){\

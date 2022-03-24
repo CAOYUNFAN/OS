b570ba5ccce97fd03142064a18a2cb72a4da983f
tracer-nju <tracer@nju.edu.cn>
2022-03-25 03:01:26 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:01:26 up  3:19,  1 user,  load average: 0.66, 0.80, 0.76

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -277,7 +277,7 @@ static inline free_list * update(free_list ** head){
 static inline void kfree_rest(void * ptr){
   uintptr_t len=LOWBIT((uintptr_t)ptr);
   for(;len;len>>=1){
-    if((uintptr_t)ptr+len<=heap_rest_end&&MTG_addr(ptr,len)->magic|MAGIC_MTG==MAGIC_MTG&&MTG_addr(ptr,len)->size==len) break;
+    if((uintptr_t)ptr+len<=heap_rest_end&&(MTG_addr(ptr,len)->magic|MAGIC_MTG)==MAGIC_MTG&&MTG_addr(ptr,len)->size==len) break;
   }
   DEBUG(memset((void *)ptr,MAGIC_UNUSED,len));
   int pos=0;

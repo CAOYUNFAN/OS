97799f6eca77e904c668231b82b2a48dcb19ec1b
tracer-nju <tracer@nju.edu.cn>
2022-03-25 03:06:39 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:06:39 up  3:24,  1 user,  load average: 0.54, 0.72, 0.73

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -4,15 +4,15 @@
 #ifdef TEST
   #include <assert.h>
   #include <stdio.h>
-  #define DEBUG(...) (__VA_ARGS__)
+  #define DEBUG(...) __VA_ARGS__
   #define Assert(cond,format,...)\
     ((void) sizeof ((cond) ? 1 : 0),__extension__({if(!(cond)){\
         printf(format,__VA_ARGS__);\
         assert(cond);\
     }}))
 #else
-  #define DEBUG(...) (0)
-  #define Assert(...) (0)
+  #define DEBUG(...) 0;
+  #define Assert(...) 0;
 #endif
 
 
@@ -76,6 +76,219 @@ CAO_FREE(128)
 CAO_FIXED_INIT(4096,heap_128_end,total_num/4*Unit_size)
 CAO_ALLOC(4096)
 CAO_FREE(4096)
+
+static uintptr_t heap_rest_start,heap_rest_end;
+static free_list ** start_of_rest;
+static int lock_rest;
+void init_rest(){
+  heap_rest_start=heap_4096_end;heap_rest_end=HEAP_END-Unit_size;
+  uintptr_t num=0;
+  for(uintptr_t i=4096;i<=(MAX_malloc);i<<=1) num++;
+  num++;
+  start_of_rest=kernel_alloc(sizeof(free_list *)*num);
+  uintptr_t j=0;
+  for(uintptr_t i=4096;i<MAX_malloc;i<<=1,++j) start_of_rest[j]=NULL;
+  start_of_rest[j]=NULL;
+  if(heap_rest_end%(MAX_malloc<<1)!=0){
+    start_of_rest[j]=(free_list *)(heap_rest_end-MAX_malloc);
+    start_of_rest[j]->size=MAX_malloc;
+    start_of_rest[j]->nxt=NULL;
+  }
+  if(heap_rest_start%(MAX_malloc<<1)!=0){
+    free_list * temp=(free_list *)heap_rest_start;
+    temp->size=MAX_malloc;temp->nxt=start_of_rest[j];
+    start_of_rest[j]=temp;
+    printf("BEGIN:%p->%p->%p\n",start_of_rest[j],start_of_rest[j]->nxt,start_of_rest[j]->nxt->nxt);
+  }
+  ++j;
+  if(ROUNDUP(heap_rest_start,MAX_malloc<<1)==ROUNDDOWN(heap_rest_end,MAX_malloc<<1)) start_of_rest[j]=NULL;
+  else{
+    start_of_rest[j]=(free_list *)ROUNDUP(heap_rest_start,MAX_malloc<<1);
+    start_of_rest[j]->size=MAX_malloc<<1;
+    for(uintptr_t ptr=ROUNDUP(heap_rest_start,MAX_malloc<<1)+(MAX_malloc<<1);ptr<ROUNDDOWN(heap_rest_end,MAX_malloc<<1);ptr+=MAX_malloc<<1){
+      ((free_list *)ptr)->size=MAX_malloc<<1;
+      ((free_list *)(ptr-(MAX_malloc<<1)))->nxt=(free_list *)ptr;
+    }
+    ((free_list *)(ROUNDDOWN(heap_rest_end,MAX_malloc<<1)-(MAX_malloc<<1)))->nxt=NULL;
+  }  
+}
+static inline void insert(free_list * insert,free_list ** head){
+  Assert(*head==NULL||insert->size==(*head)->size,"INSERT:%p->%ld,%p->%ld\n",insert,insert->size,*head,(*head)->size);
+  if(*head==NULL||(uintptr_t)*head>(uintptr_t)insert){
+    insert->nxt=*head;
+    *head=insert;
+    return;
+  }
+  for(free_list * now=*head;now!=NULL;now=now->nxt)
+  if((uintptr_t)now<(uintptr_t)insert&&(now->nxt==NULL||(uintptr_t)now->nxt>(uintptr_t)insert)){
+    insert->nxt=now->nxt;
+    now->nxt=insert;
+    return;
+  }
+}
+static inline void * kalloc_rest(size_t size){
+  size+=sizeof(mem_tag);
+  free_list * ret=NULL;
+  spin_lock(&lock_rest);
+  for(uintptr_t i=4096,j=0;i<=MAX_malloc<<1;i<<=1,++j){
+    if(i<size||!start_of_rest[j]) continue;
+    ret=start_of_rest[j];
+    start_of_rest[j]=start_of_rest[j]->nxt;
+    #ifdef TEST
+    unsigned long jj=0;
+    if(!(ret->nxt==NULL||ret->nxt->size==i)) printf("%p:%ld-%p:%ld\n",ret,i,ret->nxt,ret->nxt->size);
+    assert(ret->nxt==NULL||ret->nxt->size==i);
+    for(unsigned char * ptr=((unsigned char *)ret)+sizeof(free_list);jj<i-sizeof(free_list);++ptr,++jj) {
+      if(*ptr!=MAGIC_UNUSED) {
+        printf("%p-%lx,size=%ld\n",ptr,jj,size);
+        printf("%c\n",*ptr);
+        printf("%p\n",((free_list *)ret)->nxt);
+      }
+      assert(*ptr==MAGIC_UNUSED);
+    }
+    #endif
+    for(;(i>>1)>=size;i>>=1){
+      ret->size>>=1;
+      free_list * divide=(free_list *)((uintptr_t)ret+ret->size);
+      divide->size=(i>>1);
+      insert(divide,&start_of_rest[--j]);
+    }
+    DEBUG(memset(ret,MAGIC_USED,i);)
+    MTG_addr(ret,i)->size=i;MTG_addr(ret,i)->magic=MAGIC_MTG;
+    break;
+  }
+  spin_unlock(&lock_rest);
+  return (void *)ret;
+}
+static inline free_list * update(free_list ** head){
+  if(*head==NULL) return NULL;
+  uintptr_t len=(*head)->size;
+  if(len==MAX_malloc) return NULL;
+  if(LOWBIT((uintptr_t)*head)>len&&(uintptr_t)*head+len==(uintptr_t)((*head)->nxt)){
+    free_list * ret=*head;
+    *head=(*head)->nxt->nxt;
+    ret->size=(len<<1);
+    #ifdef TEST
+    memset((void *)ret->nxt,MAGIC_UNUSED,sizeof(free_list));
+    int jj=0;
+    for(unsigned char *x=((unsigned char *)ret+sizeof(free_list));jj<(len<<1)-sizeof(free_list);++jj,++x) {
+      if(*x!=MAGIC_UNUSED) printf("%p-%lx\n",x,jj+sizeof(free_list));
+      assert(*x==MAGIC_UNUSED);
+    }
+    #endif
+    return ret;
+  }
+  for(free_list * now=*head;now->nxt!=NULL;now=now->nxt){
+    free_list * next=now->nxt;
+    if(LOWBIT((uintptr_t)next)>len&&(uintptr_t)next+len==(uintptr_t)(next->nxt)){
+      now->nxt=next->nxt->nxt;
+      next->size=len<<1;
+      DEBUG(memset(next->nxt,MAGIC_UNUSED,sizeof(free_list));)
+      return next;
+    }
+  }
+  return NULL;
+}
+static inline void kfree_rest(void * ptr){
+  uintptr_t len=LOWBIT((uintptr_t)ptr);
+  for(;len;len>>=1){
+    if((uintptr_t)ptr+len<=heap_rest_end&&(MTG_addr(ptr,len)->magic|MAGIC_MTG)==MAGIC_MTG&&MTG_addr(ptr,len)->size==len) break;
+  }
+  DEBUG(memset((void *)ptr,MAGIC_UNUSED,len);)
+  int pos=0;
+  for(uintptr_t i=4096;i<len;i<<=1) ++pos;
+  ((free_list *)ptr)->size=len;
+  spin_lock(&lock_rest);
+  insert(ptr,&start_of_rest[pos]);
+  while((ptr=update(&start_of_rest[pos]))!=NULL) insert(ptr,&start_of_rest[++pos]);
+  spin_unlock(&lock_rest);
+}
+
+void init_mm(){
+  sbrk_now=HEAP_END;
+  DEBUG(memset((void *)HEAP_START,MAGIC_UNUSED,HEAP_END-HEAP_START);)
+  init_128();init_4096();init_rest();
+}
+
+#ifndef TEST
+static void pmm_init() {
+  uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  init_mm();
+  return;
+}
+#else
+extern FILE * fd;
+#define HEAP_SIZE 128*1024*1024
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+  fprintf(fd,"Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+  init_mm();
+  printf("num:%lx\n",total_num);
+  printf("128:[%lx,%lx) 4096:[%lx,%lx) rest: [%lx,%lx)\n",heap_128_start,heap_128_end,heap_4096_start,heap_4096_end,heap_rest_start,heap_rest_end);
+//  printf("Initialize memory Completed!\n");
+  return;
+}
+#endif
+
+static void * kalloc(size_t size){
+  if(size>MAX_malloc) return NULL;
+  if(size<=128) return kalloc_128();
+  if(size<=4096) return kalloc_4096();
+  return kalloc_rest(size);
+}
+
+static void kfree(void * ptr){
+  if((uintptr_t)ptr>=heap_128_start&&(uintptr_t)ptr<heap_128_end) kfree_128(ptr);
+  if((uintptr_t)ptr>=heap_4096_start&&(uintptr_t)ptr<heap_4096_end) kfree_4096(ptr);
+  if((uintptr_t)ptr>=heap_rest_start&&(uintptr_t)ptr<heap_rest_end) kfree_rest(ptr);
+}
+
+MODULE_DEF(pmm) = {
+  .init  = pmm_init,
+  .alloc = kalloc,
+  .free  = kfree,
+};
+
+
+/*  num_of_block=(HEAP_END-HEAP_START)/Unit_size;
+  for(uintptr_t i=HEAP_START,j=0;i<HEAP_END;i+=Unit_size,j++){
+//    printf("%lx,%lx,i=%lx,j=%d,num=%d\n",HEAP_USE_START,HEAP_END,i,j,num_of_block);
+    *lock_addr(j)=MAGIC_LOCKED;
+    start_of_free_list(j)=(free_list *)i;
+    memset((void *)i,MAGIC_UNUSED,Unit_size);
+    ((free_list *)i)->size=Unit_size;
+    ((free_list *)i)->nxt=NULL;
+    *lock_addr(j)=MAGIC_UNLOCKED;
+  }*/
+
+/*  size=up_bound(size+sizeof(mem_tag));
+  uintptr_t try_num=0;
+  for(uintptr_t i=0,j=HEAP_START;try_num<num_of_block;i=(i+1)%num_of_block,j=(j+Unit_size==HEAP_END?HEAP_START:j+HEAP_END))
+  if(atomic_xchg(lock_addr(i),MAGIC_LOCKED)==MAGIC_UNLOCKED){
+    ++try_num;
+    void * ret=find(size);
+    if(ret) work(ret,size);
+    spin_unlock(lock_addr(i));
+    if(ret) return ret;
+  }
+  return NULL;*/
+
+/*  uintptr_t pos=(ROUNDDOWN((uintptr_t)ptr,Unit_size)-HEAP_START)/Unit_size;
+  spin_lock(lock_addr(pos));
+  real_free((uintptr_t)ptr);
+  spin_unlock(lock_addr(pos));*/
+
+  /*uintptr_t len=LOWBIT(ptr);
+  for(;len;len>>=1){
+    uintptr_t pos=ptr+len-sizeof(mem_tag);
+    if(((mem_tag *)pos)->size+ptr==pos+sizeof(mem_tag)&&((mem_tag *)pos)->magic==MAGIC_MHD) break;
+  }
+  memset((void *)ptr,len,MAGIC_UNUSED);*/
+
 
 /*
 static free_list * start_of_128;
@@ -161,215 +374,3 @@ static inline void kfree_4096(void * ptr){
   start_of_4096=hdr;
   spin_unlock(&lock_4096);
 }*/
-
-static uintptr_t heap_rest_start,heap_rest_end;
-static free_list ** start_of_rest;
-static int lock_rest;
-void init_rest(){
-  heap_rest_start=heap_4096_end;heap_rest_end=HEAP_END-Unit_size;
-  uintptr_t num=0;
-  for(uintptr_t i=4096;i<=(MAX_malloc);i<<=1) num++;
-  num++;
-  start_of_rest=kernel_alloc(sizeof(free_list *)*num);
-  uintptr_t j=0;
-  for(uintptr_t i=4096;i<MAX_malloc;i<<=1,++j) start_of_rest[j]=NULL;
-  start_of_rest[j]=NULL;
-  if(heap_rest_end%(MAX_malloc<<1)!=0){
-    start_of_rest[j]=(free_list *)(heap_rest_end-MAX_malloc);
-    start_of_rest[j]->size=MAX_malloc;
-    start_of_rest[j]->nxt=NULL;
-  }
-  if(heap_rest_start%(MAX_malloc<<1)!=0){
-    free_list * temp=(free_list *)heap_rest_start;
-    temp->size=MAX_malloc;temp->nxt=start_of_rest[j];
-    start_of_rest[j]=temp;
-    printf("BEGIN:%p->%p->%p\n",start_of_rest[j],start_of_rest[j]->nxt,start_of_rest[j]->nxt->nxt);
-  }
-  ++j;
-  if(ROUNDUP(heap_rest_start,MAX_malloc<<1)==ROUNDDOWN(heap_rest_end,MAX_malloc<<1)) start_of_rest[j]=NULL;
-  else{
-    start_of_rest[j]=(free_list *)ROUNDUP(heap_rest_start,MAX_malloc<<1);
-    start_of_rest[j]->size=MAX_malloc<<1;
-    for(uintptr_t ptr=ROUNDUP(heap_rest_start,MAX_malloc<<1)+(MAX_malloc<<1);ptr<ROUNDDOWN(heap_rest_end,MAX_malloc<<1);ptr+=MAX_malloc<<1){
-      ((free_list *)ptr)->size=MAX_malloc<<1;
-      ((free_list *)(ptr-(MAX_malloc<<1)))->nxt=(free_list *)ptr;
-    }
-    ((free_list *)(ROUNDDOWN(heap_rest_end,MAX_malloc<<1)-(MAX_malloc<<1)))->nxt=NULL;
-  }  
-}
-static inline void insert(free_list * insert,free_list ** head){
-  Assert(*head==NULL||insert->size==(*head)->size,"INSERT:%p->%ld,%p->%ld\n",insert,insert->size,*head,(*head)->size);
-  if(*head==NULL||(uintptr_t)*head>(uintptr_t)insert){
-    insert->nxt=*head;
-    *head=insert;
-    return;
-  }
-  for(free_list * now=*head;now!=NULL;now=now->nxt)
-  if((uintptr_t)now<(uintptr_t)insert&&(now->nxt==NULL||(uintptr_t)now->nxt>(uintptr_t)insert)){
-    insert->nxt=now->nxt;
-    now->nxt=insert;
-    return;
-  }
-}
-static inline void * kalloc_rest(size_t size){
-  size+=sizeof(mem_tag);
-  free_list * ret=NULL;
-  spin_lock(&lock_rest);
-  for(uintptr_t i=4096,j=0;i<=MAX_malloc<<1;i<<=1,++j){
-    if(i<size||!start_of_rest[j]) continue;
-    ret=start_of_rest[j];
-    start_of_rest[j]=start_of_rest[j]->nxt;
-    #ifdef TEST
-    unsigned long jj=0;
-    if(!(ret->nxt==NULL||ret->nxt->size==i)) printf("%p:%ld-%p:%ld\n",ret,i,ret->nxt,ret->nxt->size);
-    assert(ret->nxt==NULL||ret->nxt->size==i);
-    for(unsigned char * ptr=((unsigned char *)ret)+sizeof(free_list);jj<i-sizeof(free_list);++ptr,++jj) {
-      if(*ptr!=MAGIC_UNUSED) {
-        printf("%p-%lx,size=%ld\n",ptr,jj,size);
-        printf("%c\n",*ptr);
-        printf("%p\n",((free_list *)ret)->nxt);
-      }
-      assert(*ptr==MAGIC_UNUSED);
-    }
-    #endif
-    for(;(i>>1)>=size;i>>=1){
-      ret->size>>=1;
-      free_list * divide=(free_list *)((uintptr_t)ret+ret->size);
-      divide->size=(i>>1);
-      insert(divide,&start_of_rest[--j]);
-    }
-    DEBUG(memset(ret,MAGIC_USED,i));
-    MTG_addr(ret,i)->size=i;MTG_addr(ret,i)->magic=MAGIC_MTG;
-    break;
-  }
-  spin_unlock(&lock_rest);
-  return (void *)ret;
-}
-static inline free_list * update(free_list ** head){
-  if(*head==NULL) return NULL;
-  uintptr_t len=(*head)->size;
-  if(len==MAX_malloc) return NULL;
-  if(LOWBIT((uintptr_t)*head)>len&&(uintptr_t)*head+len==(uintptr_t)((*head)->nxt)){
-    free_list * ret=*head;
-    *head=(*head)->nxt->nxt;
-    ret->size=(len<<1);
-    #ifdef TEST
-    memset((void *)ret->nxt,MAGIC_UNUSED,sizeof(free_list));
-    int jj=0;
-    for(unsigned char *x=((unsigned char *)ret+sizeof(free_list));jj<(len<<1)-sizeof(free_list);++jj,++x) {
-      if(*x!=MAGIC_UNUSED) printf("%p-%lx\n",x,jj+sizeof(free_list));
-      assert(*x==MAGIC_UNUSED);
-    }
-    #endif
-    return ret;
-  }
-  for(free_list * now=*head;now->nxt!=NULL;now=now->nxt){
-    free_list * next=now->nxt;
-    if(LOWBIT((uintptr_t)next)>len&&(uintptr_t)next+len==(uintptr_t)(next->nxt)){
-      now->nxt=next->nxt->nxt;
-      next->size=len<<1;
-      DEBUG(memset(next->nxt,MAGIC_UNUSED,sizeof(free_list)));
-      return next;
-    }
-  }
-  return NULL;
-}
-static inline void kfree_rest(void * ptr){
-  uintptr_t len=LOWBIT((uintptr_t)ptr);
-  for(;len;len>>=1){
-    if((uintptr_t)ptr+len<=heap_rest_end&&(MTG_addr(ptr,len)->magic|MAGIC_MTG)==MAGIC_MTG&&MTG_addr(ptr,len)->size==len) break;
-  }
-  DEBUG(memset((void *)ptr,MAGIC_UNUSED,len));
-  int pos=0;
-  for(uintptr_t i=4096;i<len;i<<=1) ++pos;
-  ((free_list *)ptr)->size=len;
-  spin_lock(&lock_rest);
-  insert(ptr,&start_of_rest[pos]);
-  while((ptr=update(&start_of_rest[pos]))!=NULL) insert(ptr,&start_of_rest[++pos]);
-  spin_unlock(&lock_rest);
-}
-
-void init_mm(){
-  sbrk_now=HEAP_END;
-  DEBUG(memset((void *)HEAP_START,MAGIC_UNUSED,HEAP_END-HEAP_START));
-  init_128();init_4096();init_rest();
-}
-
-#ifndef TEST
-static void pmm_init() {
-  uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  init_mm();
-  return;
-}
-#else
-extern FILE * fd;
-#define HEAP_SIZE 128*1024*1024
-static void pmm_init() {
-  char *ptr  = malloc(HEAP_SIZE);
-  heap.start = ptr;
-  heap.end   = ptr + HEAP_SIZE;
-  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
-  fprintf(fd,"Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
-  init_mm();
-  printf("num:%lx\n",total_num);
-  printf("128:[%lx,%lx) 4096:[%lx,%lx) rest: [%lx,%lx)\n",heap_128_start,heap_128_end,heap_4096_start,heap_4096_end,heap_rest_start,heap_rest_end);
-//  printf("Initialize memory Completed!\n");
-  return;
-}
-#endif
-
-static void * kalloc(size_t size){
-  if(size>MAX_malloc) return NULL;
-  if(size<=128) return kalloc_128();
-  if(size<=4096) return kalloc_4096();
-  return kalloc_rest(size);
-}
-
-static void kfree(void * ptr){
-  if((uintptr_t)ptr>=heap_128_start&&(uintptr_t)ptr<heap_128_end) kfree_128(ptr);
-  if((uintptr_t)ptr>=heap_4096_start&&(uintptr_t)ptr<heap_4096_end) kfree_4096(ptr);
-  if((uintptr_t)ptr>=heap_rest_start&&(uintptr_t)ptr<heap_rest_end) kfree_rest(ptr);
-}
-
-MODULE_DEF(pmm) = {
-  .init  = pmm_init,
-  .alloc = kalloc,
-  .free  = kfree,
-};
-
-
-/*  num_of_block=(HEAP_END-HEAP_START)/Unit_size;
-  for(uintptr_t i=HEAP_START,j=0;i<HEAP_END;i+=Unit_size,j++){
-//    printf("%lx,%lx,i=%lx,j=%d,num=%d\n",HEAP_USE_START,HEAP_END,i,j,num_of_block);
-    *lock_addr(j)=MAGIC_LOCKED;
-    start_of_free_list(j)=(free_list *)i;
-    memset((void *)i,MAGIC_UNUSED,Unit_size);
-    ((free_list *)i)->size=Unit_size;
-    ((free_list *)i)->nxt=NULL;
-    *lock_addr(j)=MAGIC_UNLOCKED;
-  }*/
-
-/*  size=up_bound(size+sizeof(mem_tag));
-  uintptr_t try_num=0;
-  for(uintptr_t i=0,j=HEAP_START;try_num<num_of_block;i=(i+1)%num_of_block,j=(j+Unit_size==HEAP_END?HEAP_START:j+HEAP_END))
-  if(atomic_xchg(lock_addr(i),MAGIC_LOCKED)==MAGIC_UNLOCKED){
-    ++try_num;
-    void * ret=find(size);
-    if(ret) work(ret,size);
-    spin_unlock(lock_addr(i));
-    if(ret) return ret;
-  }
-  return NULL;*/
-
-/*  uintptr_t pos=(ROUNDDOWN((uintptr_t)ptr,Unit_size)-HEAP_START)/Unit_size;
-  spin_lock(lock_addr(pos));
-  real_free((uintptr_t)ptr);
-  spin_unlock(lock_addr(pos));*/
-
-  /*uintptr_t len=LOWBIT(ptr);
-  for(;len;len>>=1){
-    uintptr_t pos=ptr+len-sizeof(mem_tag);
-    if(((mem_tag *)pos)->size+ptr==pos+sizeof(mem_tag)&&((mem_tag *)pos)->magic==MAGIC_MHD) break;
-  }
-  memset((void *)ptr,len,MAGIC_UNUSED);*/

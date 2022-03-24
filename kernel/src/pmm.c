8260873a96ffaca9bb1a2eaee7db6a3f9ef5c223
tracer-nju <tracer@nju.edu.cn>
2022-03-25 01:45:33 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:45:33 up  2:12,  1 user,  load average: 0.79, 0.62, 0.50

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -179,7 +179,7 @@ void init_rest(){
   else{
     start_of_rest[j]=(free_list *)ROUNDUP(heap_rest_start,MAX_malloc<<1);
     ((free_list *)heap_rest_start)->size=MAX_malloc<<1;
-    for(uintptr_t ptr=ROUNDUP(heap_rest_start,MAX_malloc<<1)+(MAX_malloc<<1);ptr<heap_rest_end;ptr+=MAX_malloc<<1){
+    for(uintptr_t ptr=ROUNDUP(heap_rest_start,MAX_malloc<<1)+(MAX_malloc<<1);ptr<ROUNDDOWN(heap_rest_end,MAX_malloc<<1);ptr+=MAX_malloc<<1){
       ((free_list *)ptr)->size=MAX_malloc<<1;
       ((free_list *)(ptr-(MAX_malloc<<1)))->nxt=(free_list *)ptr;
     }

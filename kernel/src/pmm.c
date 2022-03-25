cf6d0ff27b2eab594103d8c3090bd5fe8d130387
tracer-nju <tracer@nju.edu.cn>
2022-03-26 04:18:32 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:18:32 up  2:46,  1 user,  load average: 0.40, 0.79, 0.80

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,63 +1,30 @@
 #include <common.h>
-#include "macros_for_pmm.h"
 
-#define MAX_alloc (16*1024*1024)
-#define Unit_size (4096)
-#define HEAP_START ROUNDUP((uintptr_t)heap.start,Unit_size)
-#define HEAP_END ROUNDDOWN((uintptr_t)heap.end,Unit_size)
-#define HEAP_REAL_START (HEAP_START+Unit_size)
-#define total_num ((HEAP_END-HEAP_START)/Unit_size)
+static uintptr_t top;
+static int lock;
 
-static uintptr_t kernel_max; 
-static void * kernel_alloc(size_t size){
-    void * ret=(void *)kernel_max;
-    kernel_max+=size;
-    Assert(kernel_max<=HEAP_REAL_START,"TOO MUCH kernel use! %ld\n",size);
-    return ret;
+typedef int spinlock_t;
+
+#define MAGIC_UNLOCKED (0)
+#define MAGIC_LOCKED (1)
+#define LOWBIT(x) ((x)&((x)^((x)-1)))
+
+static inline void spin_lock(spinlock_t *lk) {
+  while (1) {
+    intptr_t value = atomic_xchg(lk, MAGIC_LOCKED);
+    if (value == MAGIC_UNLOCKED) {
+      break;
+    }
+  }
 }
 
-static free_list ** head;
-typedef struct{
-    free_list * header;
-    int num;
-}info_start;
-
-static info_start * head_64;
-static info_start * head_128;
-static info_start * head_4096;
-
-static inline void init_work(){
-    int num=0;
-    for(int i=4096;i<=(MAX_alloc<<1);i<<=1) ++num;
-    head=kernel_alloc(sizeof(free_list *)*num);
-    memset(head,0,sizeof(free_list *)*num);
-    uintptr_t now=HEAP_END,len=4096,cen=0;
-    for(;(now-len)>=HEAP_START&&LOWBIT(now)<(MAX_alloc<<1);len<<=1,++cen)
-    if(LOWBIT(now)==len){
-        free_list* temp=(free_list *)(now-len);
-        temp->size=len;temp->nxt=head[cen];head[cen]=temp;
-        now-=len;
-    }
-    for(;now-len>=HEAP_REAL_START;now-=len){
-        Assert(len==(MAX_alloc<<1)&&cen==(num-1),"%s\n","Initialization Errors!");
-        free_list * temp=(free_list *)(now-len);
-        temp->size=len;temp->nxt=head[cen];head[cen]=temp;
-    }
-    for(;now>HEAP_REAL_START;len>>=1,--cen)
-    if(now-len>=HEAP_REAL_START){
-        free_list * temp=(free_list *)(now-len);
-        temp->size=len;temp->nxt=head[cen];head[cen]=temp;
-        now-=len;
-    }
+static inline void spin_unlock(spinlock_t *lk) {
+  atomic_xchg(lk, MAGIC_UNLOCKED);
 }
 
-static inline void init_mm(){
-    kernel_max=HEAP_START;
-    DEBUG(memset(HEAP_START,MAGIC_UNUSED,HEAP_END-HEAP_START));
-    init_work();
-    head_64=kernel_alloc(sizeof(info_start));head_64->header=NULL;head_64->num=0;
-    head_128=kernel_alloc(sizeof(info_start));head_128->header=NULL;head_128->num=0;
-    head_4096=kernel_alloc(sizeof(info_start));head_4096->header=NULL;head_4096->num=0;
+void init_mm(){
+  top=(uintptr_t)heap.start;
+  lock=MAGIC_UNLOCKED;
 }
 
 #ifndef TEST
@@ -69,7 +36,7 @@ static void pmm_init() {
 }
 #else
 extern FILE * fd;
-#define HEAP_SIZE (128*1024*1024+Unit_size)
+#define HEAP_SIZE (512*1024*1024+Unit_size)
 static void pmm_init() {
   char *ptr  = malloc(HEAP_SIZE);
   heap.start = ptr;
@@ -86,12 +53,16 @@ static void pmm_init() {
 
 
 static void * kalloc(size_t size){
-    if(size>MAX_alloc) return NULL;
-    return NULL;
+  uintptr_t ret=top;
+  spin_lock(&lock);
+  while (ret<(uintptr_t)heap.end&&LOWBIT(ret)<size) ret+=LOWBIT(ret);
+  if(ret+size>(uintptr_t)heap.end) ret=0;
+  else top=ret+size;
+  spin_unlock(&lock);
+  return (void *) ret;
 }
 
 static void kfree(void * ptr){
-
 }
 
 MODULE_DEF(pmm) = {

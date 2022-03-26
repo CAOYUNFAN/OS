2d2a52026f48bf6cf42ce0edc5c0e84fcad00628
tracer-nju <tracer@nju.edu.cn>
2022-03-26 20:54:37 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:54:37 up  3:55,  1 user,  load average: 0.70, 0.88, 0.78

--- kernel/src/buddy.c
+++ kernel/src/buddy.c
@@ -49,8 +49,9 @@ void * buddy_alloc(buddy * self,size_t size){
 
     void * ret=(void *)(HEAP_OFFSET_START+offset*Unit_size);
     #ifdef TEST
+        printf("ALLOC_PAGE:%p-%ld\n",ret,size*Unit_size);
         int i=0;
-        for(unsigned char * ptr=(unsigned char *)ret;i<size*Unit_size;i++,ptr++) Assert(*ptr==MAGIC_UNUSED,"%p=%x\n",ptr,*ptr);
+        for(unsigned char * ptr=(unsigned char *)ret;i<size*Unit_size;i++,ptr++) Assert(*ptr==MAGIC_UNUSED,"Unexpected Magic%p=%x\n",ptr,*ptr);
         memset(ret,MAGIC_BIG,size*Unit_size);
     #endif
     return ret;

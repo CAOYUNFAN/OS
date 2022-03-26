a4b399d75d079c6f2210aa91cd9115fa3dff0ee7
tracer-nju <tracer@nju.edu.cn>
2022-03-26 20:43:44 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:43:44 up  3:44,  1 user,  load average: 0.64, 0.61, 0.55

--- kernel/src/buddy.c
+++ kernel/src/buddy.c
@@ -15,7 +15,7 @@ buddy * buddy_init(size_t size){
     buddy * self=(buddy *)kernel_alloc(2*size*sizeof(int));
     self->size=size;
 
-    uintptr_t heap_real_start=ROUNDUP(HEAP_START+kernel_max,Unit_size),heap_real_end=ROUNDDOWN(HEAP_END,Unit_size);
+    uintptr_t heap_real_start=ROUNDUP(kernel_max,Unit_size),heap_real_end=ROUNDDOWN(HEAP_END,Unit_size);
     printf("%lx %lx %lx\n",heap_real_start,heap_real_end,HEAP_OFFSET_START);
     printf("%lx %lx\n",Unit_size*size,size);
     for(size_t i=size,j=HEAP_OFFSET_START;i<2*size;i++,j+=Unit_size){

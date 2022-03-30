01fb2c891d34ee451bf4241c80f31ae449d74690
tracer-nju <tracer@nju.edu.cn>
2022-03-31 03:18:19 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:18:19 up  4:34,  1 user,  load average: 0.11, 0.35, 0.47

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -116,6 +116,7 @@ static inline void * kalloc_small(start_info * head,size_t size,start_info_all *
   spin_unlock(&head->lock);
   #ifdef TEST
     if(ret){
+      printf("%p\n",ret);
       unsigned char * i=((unsigned char *)ret)+sizeof(free_list);
       for(uintptr_t j=sizeof(free_list);j<size;i++,j++) Assert(*i==MAGIC_BIG,"Unexpeted Magic %p=%x\n",i,*i);
       memset(ret,MAGIC_SMALL,size);

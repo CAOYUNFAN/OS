4588c1778ea466df15d423ba2b209cc20e911652
tracer-nju <tracer@nju.edu.cn>
2022-04-29 03:29:45 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:29:45 up  1:14,  1 user,  load average: 0.46, 0.50, 0.47

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -7,10 +7,19 @@
 char * file_templelate ="int __expr_wrapper_%d () { return %s ;}\n";
 char * gcc_arg[]={"gcc","-fPIC","-shared","-o",NULL,NULL,NULL};
 
-void make_link(char * data){
-  char name[]="/tmp/filename-XXXXXX";
-  int tt=mkstemp(name);
+void make_tmp_file(char * name,char * name_so){
+  int tt=mkstemps(name,2);
   assert(tt>=0);
+  int i=0;
+  for(;name[i]&&name[i]!='c';i++) name_so[i]=name[i];
+  name_so[i]='s';name_so[i+1]='o';name_so[i+2]=0;
+  return;
+}
+
+void make_link(char * data){
+  char name[]="/tmp/filename-XXXXXX.c";
+  char name_so[25];
+  make_tmp_file(name,name_so);
   FILE * fd=fopen(name,"w");assert(fd);
   #ifdef LOCAL
   printf("NAME=\n%s\n",name);
@@ -18,10 +27,8 @@ void make_link(char * data){
   fputs(data,fd);
   fclose(fd);
   pid_t pid=fork();
-  char name2[25];
-  sprintf(name2,"%s.so",name);  
   if(!pid){
-    gcc_arg[4]=name2;gcc_arg[5]=name;
+    gcc_arg[4]=name_so;gcc_arg[5]=name;
     execvp("gcc",gcc_arg);
   }
   

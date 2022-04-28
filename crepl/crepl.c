e49c5121cf37297a565766c102d878a3418449fd
tracer-nju <tracer@nju.edu.cn>
2022-04-29 04:42:24 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:42:24 up  2:06,  1 user,  load average: 0.34, 0.75, 0.71

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -4,8 +4,11 @@
 #include <assert.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <dlfcn.h>
 
-char * file_templelate ="int __expr_wrapper_%d () { return %s ;}\n";
+char * file_templelate = "int __expr_wrapper_%d () { return %s ;}\n";
+char * func_templelate = "__expr_wrapper_%d";
 char * gcc_arg[]={"gcc","-fPIC","-shared","-o",NULL,NULL,NULL};
 
 void make_tmp_file(char * name,char * name_so){
@@ -17,7 +20,7 @@ void make_tmp_file(char * name,char * name_so){
   return;
 }
 
-void make_link(char * data){
+void * make_link(char * data){
   char name[]="filename-XXXXXX.c";
   char name_so[25];
   make_tmp_file(name,name_so);
@@ -30,24 +33,54 @@ void make_link(char * data){
   pid_t pid=fork();
   if(!pid){
     gcc_arg[4]=name_so;gcc_arg[5]=name;
+    int fdd=open("/dev/null",O_WRONLY);
+    dup2(fdd,STDOUT_FILENO);
+    dup2(fdd,STDERR_FILENO);
     execvp("gcc",gcc_arg);
     assert(0);
   }
   int wstauts;
   waitpid(pid,&wstauts,0);
+  if(!WIFEXITED(wstauts)){
+    printf("Compile Error!\n");
+    return NULL;
+  }
+  #ifdef LOCAL
+  printf("Compile Completed!\n");
+  #endif
+  return dlopen(name_so,RTLD_NOW|RTLD_DEEPBIND);
 }
+
+char func_main[4096],func_name[4096];
+
+typedef int (*)() my_func;
 
 int main(int argc, char *argv[]) {
   static char line[4096];
+  int x=0;
   while (1) {
+    
     printf("crepl> ");
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
-    make_link(line);
     #ifdef LOCAL
-    printf("Got %zu chars.\n", strlen(line)); // ??
+    printf("%s\nGot %zu chars.\n", line,strlen(line)); // ??
     #endif
+    if(strncmp(line,"int ",4)==0){
+      if(make_link(line)) puts("OK.");
+    }else{
+      sprintf(func_main,file_templelate,x,line);
+      void * handle=make_link(func_main);
+      if(!handle) continue;
+      sprintf(func_name,func_templelate,x);
+      my_func ans_func=dlsym(handle,func_name);
+      printf("%d\n"ans_func());
+    }
+    x++;
   }
+  #ifdef LOCAL
+  putchar('\n');
+  #endif
 }

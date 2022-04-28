f8f72794432e2aaba825d89ec31b05d1e90709ab
tracer-nju <tracer@nju.edu.cn>
2022-04-29 02:42:13 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:42:13 up 46 min,  1 user,  load average: 0.39, 0.48, 0.62

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+char * file_templelate ="int __expr_wrapper_%d () { return %s ;}\n";
+char * gcc_arg []={"gcc",'-fPIC',"-shared",NULL,NULL,NULL};
+
+void make_link(char * data){
+  char name[]="filename-XXXXXX";
+  int tt=mkstemp(name);
+  assert(tt>=0);
+  FILE * fd=fopen(name,"r");assert(fd);
+  #ifdef LOCAL
+  printf("NAME=\n%s\n",name);
+  #endif
+  fputs(data,fd);
+  fclose(fd);
+  pid_t pid=fork();
+  if(!pid){
+    char name2[20];
+    fprintf(name2,"%s.so",name);  
+    execlp("gcc","-fPIC","-shared","-o",name2,name,NULL);
+  }
+  
+}
 
 int main(int argc, char *argv[]) {
   static char line[4096];
@@ -9,6 +34,9 @@ int main(int argc, char *argv[]) {
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
+    make_link(line);
+    #ifdef LOCAL
     printf("Got %zu chars.\n", strlen(line)); // ??
+    #endif
   }
 }

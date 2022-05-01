5866eee35338e016102c9cdfc44113d78c589ffc
tracer-nju <tracer@nju.edu.cn>
2022-05-02 06:11:16 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:11:16 up  1:29,  1 user,  load average: 0.31, 0.58, 0.62

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -62,7 +62,7 @@ static inline task_t * del_list2(list_head * list){
 static Context * kmt_context_save(Event ev,Context * ctx){
 //    Log("save_context!");
     task_t * current=current_all[cpu_current()];
-    Assert(current==NULL||current->status!=TASK_RUNABLE,"the status of %p SHOULD NOT be RUNNABLE!",current);
+    Assert(current==NULL||current->status!=TASK_RUNABLE,"the status %d of %p SHOULD NOT be RUNNABLE!",current->status,current);
     if(current) current->ctx=ctx;
 //    Log("%p %p",current,ctx);
     return NULL;

46b399e96e59144fd4e1aa1906cdf22e41361a1a
tracer-nju <tracer@nju.edu.cn>
2022-05-02 05:08:08 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:08:08 up 26 min,  1 user,  load average: 0.71, 1.01, 0.95

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -17,16 +17,16 @@ static void os_init() {
   kmt->init();
 
   #ifdef LOCAL
-  char ch[20];
+  static char ch1[5][20],ch2[5][20];
   kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
   kmt->sem_init(&fill,  "fill",  0);
   for (int i = 0; i < 4; i++){
-    sprintf(ch,"producer-%d",i);
-    kmt->create(task_alloc(), ch, producer, NULL);
+    sprintf(ch1[i],"producer-%d",i);
+    kmt->create(task_alloc(), ch1[i], producer, NULL);
   } // 4 个生产者
   for (int i = 0; i < 5; i++){
-    sprintf(ch,"consumer-%d",i);
-    kmt->create(task_alloc(), ch, consumer, NULL);
+    sprintf(ch2[i],"consumer-%d",i);
+    kmt->create(task_alloc(), ch2[i], consumer, NULL);
   } // 5 个消费者
   #endif
 }

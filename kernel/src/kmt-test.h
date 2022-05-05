0bfee89cf7fa9dd3db9086ea8c1909241206c4c1
tracer-nju <tracer@nju.edu.cn>
2022-05-06 05:04:36 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:04:36 up  2:13,  1 user,  load average: 1.13, 1.00, 0.82

--- kernel/src/kmt-test.h
+++ kernel/src/kmt-test.h
@@ -3,9 +3,12 @@
 void tty_reader(void *arg) {
   putch('a');
   device_t *tty = dev->lookup(arg);
+  putch('a');
   char cmd[128], resp[128], ps[16];
   snprintf(ps, 16, "(%s) $ ", arg);
+  putch('a');
   while (1) {
+    putch('a');
     tty->ops->write(tty, 0, ps, strlen(ps));
     int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
     cmd[nread] = '\0';

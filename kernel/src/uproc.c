a7a1b3a2d7487092cfd7fad031f0195788edb4c6
Cao YUnfan <201220073@smail.nju.edu.cn>
2022-05-15 01:38:42 +0800

    basic for L3 -part2

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -2,15 +2,7 @@
 #include <syscall.h>
 
 #include "initcode.inc"
-#define UPROC_NAME(name) . name = uproc_##name ,
-#define MAP_SHARED    1
-#define MAP_PRIVATE   2
-#define MAP_UNMAP     3
-
-#define PROT_NONE   0x1
-#define PROT_READ   0x2
-#define PROT_WRITE  0x4
-
+#include "uproc.h"
 
 static void uproc_init(){
     return;

7d52519e878150247a5ce937bd2c520d9ce6b505
tracer-nju <tracer@nju.edu.cn>
2022-06-10 03:32:09 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:32:09 up  3:06,  1 user,  load average: 0.39, 0.26, 0.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -349,9 +349,9 @@ typedef struct _bmpInfo bmpInfo;
 int file_recovery(void * ptr,u32 filesize,FILE * file){
   assert(sizeof(bmpHdr)==14);assert(sizeof(bmpInfo)==40);
   bmpHdr * bmphdr=ptr;
-//  if(bmphdr->bfType!=0x4d42||bmphdr->bfSize!=filesize||bmphdr->bfReserved!=0||bmphdr->bfSize!=54) return 0;
+  if(bmphdr->bfType!=0x4d42||bmphdr->bfSize!=filesize||bmphdr->bfReserved!=0||bmphdr->bfSize!=54) return 0;
   bmpInfo * bmpinfo=OFFSET_BASIC_TYPE(14,ptr,bmpInfo *);
-//  if(bmpinfo->bisize!=40||bmpinfo->biPlanes!=1||bmpinfo->biBitCount!=24||bmpinfo->biCompression!=0||bmpinfo->biSizeImages!=filesize-54) return 0;
+  if(bmpinfo->bisize!=40||bmpinfo->biPlanes!=1||bmpinfo->biBitCount!=24||bmpinfo->biCompression!=0||bmpinfo->biSizeImages!=filesize-54) return 0;
   return 1;
 }
 /*void dummy(){

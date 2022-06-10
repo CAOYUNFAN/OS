b993e4449b2be515334ebf22f7f30159d151dc63
tracer-nju <tracer@nju.edu.cn>
2022-06-10 15:40:02 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:40:02 up  1:18,  1 user,  load average: 0.66, 0.61, 0.58

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -378,7 +378,7 @@ int chk(u8 * x,u8 * y,int len){printf("In!\n");
   LL sum=0;
   double sigma=15.0;
   for(int i=0;i<len;i++) sum+=((LL)x[i]-(LL)y[i]);
-  printf("OUT!\n");
+  printf("OUT!%d\n",len);
   if(abs(sum)<3.0*sigma*sqrt(len)) return 1;
   return 0;
 }

c6ae213ed63ca169d36acde1064c06ed44b55215
tracer-nju <tracer@nju.edu.cn>
2022-06-10 17:34:28 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:34:28 up  2:10,  1 user,  load average: 0.57, 0.78, 0.73

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -190,7 +190,7 @@ int is_bmp_hdr(void * ptr,int filesize){
   if(bmphdr->bfType!=0x4d42||bmphdr->bfSize!=filesize||bmphdr->bfReserved!=0||bmphdr->boffBits!=54) return 0;
   bmpInfo * bmpinfo=OFFSET_BASIC_TYPE(14,ptr,bmpInfo *);
   if(bmpinfo->bisize!=40||bmpinfo->biPlanes!=1||bmpinfo->biBitCount!=24||bmpinfo->biCompression!=0||bmpinfo->biSizeImages!=filesize-54) return 0;
-  if(bmpinfo->biXPelsPerMeter!=0xec4||bmpinfo->biYPelsPerMeter!=0xec4||bmpinfo->biClrUsed!=0||bmpinfo->biClrImportant!=0) return 0;
+//  if(bmpinfo->biXPelsPerMeter!=0xec4||bmpinfo->biYPelsPerMeter!=0xec4||bmpinfo->biClrUsed!=0||bmpinfo->biClrImportant!=0) return 0;
   return 1;
 }
 inline static int check_char(u8 ch){

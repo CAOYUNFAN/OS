f52f9573e706b774368c94ad6712108598148f3b
tracer-nju <tracer@nju.edu.cn>
2022-07-01 01:42:18 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:42:18 up  4:17,  1 user,  load average: 2.22, 1.86, 1.37

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -17,7 +17,7 @@ static Context * kmt_context_save(Event ev,Context * ctx){
 //    Assert(current==NULL||current->status!=TASK_RUNABLE,"the status %d of %s SHOULD NOT be RUNNABLE!",current->status,current->name);
     if(current&&current->status!=TASK_DEAD) current->ctx[current->nc++]=ctx;
     Assert(!current||current->nc==1||current->nc==2,"%s traped too much times!\n",current->name);
-    if(current) Log("save_context! name=%s(pid=%d),nc=%d",current->name,current->pid,current->nc);else Log("save_context! NULL");
+    if(current) Log("save_context! name=%s(pid=%d),nc=%d,info:%s",current->name,current->pid,current->nc,ev.msg);else Log("save_context! NULL");
 //    Log("%p %p",current,ctx);
     task_t * previous=previous_all[cpu_current()];
     if(previous){

45e5c395706b6a50d30153342921a3f23fe023d2
tracer-nju <tracer@nju.edu.cn>
2022-07-01 04:49:54 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:49:54 up  7:18,  1 user,  load average: 1.37, 1.04, 0.73

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -124,9 +124,13 @@ static void kmt_init(){
     #  define INT_MIN	(-INT_MAX - 1)
     #  define INT_MAX	2147483647
     os->on_irq(INT_MIN,EVENT_NULL,kmt_context_save);
+    printf("A\n");
     os->on_irq(INT_MAX, EVENT_NULL, kmt_schedule);
+    printf("A\n");
     os->on_irq(INT_MIN+10,EVENT_PAGEFAULT,kmt_pagefault);
+    printf("A\n");
     os->on_irq(INT_MIN+15,EVENT_SYSCALL,kmt_syscall);
+    printf("A\n");
     os->on_irq(INT_MIN+15,EVENT_ERROR,kmt_error);
     printf("A\n");
     task_queue_init(&runnable);

be410c42d1cd3417092a59e218fe8833c8324167
tracer-nju <tracer@nju.edu.cn>
2022-06-30 19:13:29 +0800

    Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:13:29 up  1:45,  1 user,  load average: 0.27, 0.42, 0.48

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -17,7 +17,7 @@ static Context * kmt_context_save(Event ev,Context * ctx){
 //    Assert(current==NULL||current->status!=TASK_RUNABLE,"the status %d of %s SHOULD NOT be RUNNABLE!",current->status,current->name);
     if(current&&current->status!=TASK_DEAD) current->ctx[current->nc++]=ctx;
     Assert(!current||current->nc==1||current->nc==2,"%s traped too much times!\n",current->name);
-    if(current) Log("save_context! rip=%p,rsp=%p,nc=%d",ctx->rip,ctx->rsp,current->nc);else Log("save_context! NULL");
+//    if(current) Log("save_context! rip=%p,rsp=%p,nc=%d",ctx->rip,ctx->rsp,current->nc);else Log("save_context! NULL");
 //    Log("%p %p",current,ctx);
     task_t * previous=previous_all[cpu_current()];
     if(previous){
@@ -74,7 +74,7 @@ static Context * kmt_schedule(Event ev,Context * ctx){
 //    Log("switch to task %s,%p",current->name,current);
     Assert(current->nc==1||current->nc==2,"%s traped too much times!",current->name);
     Context * ctx2=current->ctx[--current->nc];
-    Log("Schedule! pc=%p,switch to name %s,nc=%d",ctx2->rip,current->name,current->nc+1);
+//    Log("Schedule! pc=%p,switch to name %s,nc=%d",ctx2->rip,current->name,current->nc+1);
     return ctx2;
 }
 
